716916a3e5a5ea37e015ddce9a5a1b0b2a40e850
zehnic <96854326+zehnic@users.noreply.github.com>
2025-11-26 12:01:39 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,1 +1,14 @@
+const { Client } = require("pg");
+
+const db = new Client({
+  user: "postgres",
+  host: "localhost",
+  database: "spotify_app",
+  password: "password",
+  port: 5432,
+});
+
+db.connect();
+
+module.exports = db;
 
